0e1898725ad5327a1b8f4c3d5a418450367e9b42
Paul Droshniev <droshnevpavel@gmail.com>
2024-11-21 15:12:33 +0200

    formatter

--- index.js
+++ index.js
@@ -28,40 +28,12 @@ async function initMap() {
   const serviceStops = [
     [{ lat: 50.429147, lng: 30.560207 }, 'Музей локальних конфліктів'],
     [{ lat: 50.43069108952137, lng: 30.563733170781173 }, 'Співоче'],
-    [
-      { lat: 50.432433268220905, lng: 30.55782523788433 },
-      'Музей Ивана Гончара',
-    ],
-    [
-      { lat: 51.49683143828274, lng: 31.287428975759237 },
-      'Музей локальних конфліктів',
-    ],
+    [{ lat: 50.432433268220905, lng: 30.55782523788433 }, 'Музей Ивана Гончара',],
+    [{ lat: 51.49683143828274, lng: 31.287428975759237 }, 'Чернігів'],
   ];
-  // for (const service in serviceStops) {
-  //   // Add the circle for this city to the map.
-  //   const serviceCircle = new google.maps.Circle({
-  //     strokeColor: '#FF0000',
-  //     strokeOpacity: 0.8,
-  //     strokeWeight: 2,
-  //     fillColor: '#42c5f5',
-  //     fillOpacity: 0.35,
-  //     map,
-  //     center: position,
-  //     radius: 150000,
-  //   });
 
   // Create an info window to share between markers.
   const infoWindow = new google.maps.InfoWindow();
-  // const antennasCircle = new google.maps.Circle({
-  //   strokeColor: '#FF0000',
-  //   strokeOpacity: 0.4,
-  //   strokeWeight: 2,
-  //   fillColor: '#42c5f5',
-  //   fillOpacity: 0.2,
-  //   map: map,
-  //   center: position,
-  //   radius: 150000,
-  // });
 
   // Create the markers.
   serviceStops.forEach(([position, title], i) => {
@@ -69,7 +41,7 @@ async function initMap() {
       position,
       map,
       title: ` ${title}`,
-      // label: `${i + 1}`,
+      label: `${i + 1}`,
       optimized: false,
     });
 
