48180bc84cbe7eb2214b95452ea24ef1fbcad5fc
Paul Droshniev <droshnevpavel@gmail.com>
2024-11-21 13:59:08 +0200

    marker_center

--- index.js
+++ index.js
@@ -18,26 +18,53 @@ async function initMap() {
     mapId: 'DEMO_MAP_ID',
   });
 
-  // The marker, positioned at Uluru
+  // The marker, positioned at Kiev
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
     title: 'Kiev',
   });
 
-  const tourStops = [
+  const serviceStops = [
     [{ lat: 50.429147, lng: 30.560207 }, 'Музей локальних конфліктів'],
-    [{ lat: 50.43069108952137, lng: 30.563733170781173 }, 'Airport Mesa'],
+    [{ lat: 50.43069108952137, lng: 30.563733170781173 }, 'Співоче'],
     [
       { lat: 50.432433268220905, lng: 30.55782523788433 },
-      'Chapel of the Holy Cross',
+      'Музей Ивана Гончара',
+    ],
+    [
+      { lat: 51.49683143828274, lng: 31.287428975759237 },
+      'Музей локальних конфліктів',
     ],
   ];
+  // for (const service in serviceStops) {
+  //   // Add the circle for this city to the map.
+  //   const serviceCircle = new google.maps.Circle({
+  //     strokeColor: '#FF0000',
+  //     strokeOpacity: 0.8,
+  //     strokeWeight: 2,
+  //     fillColor: '#42c5f5',
+  //     fillOpacity: 0.35,
+  //     map,
+  //     center: position,
+  //     radius: 150000,
+  //   });
+
   // Create an info window to share between markers.
   const infoWindow = new google.maps.InfoWindow();
+  const antennasCircle = new google.maps.Circle({
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.4,
+    strokeWeight: 2,
+    fillColor: '#42c5f5',
+    fillOpacity: 0.2,
+    map: map,
+    center: position,
+    radius: 150000,
+  });
 
   // Create the markers.
-  tourStops.forEach(([position, title], i) => {
+  serviceStops.forEach(([position, title], i) => {
     const marker = new google.maps.Marker({
       position,
       map,
